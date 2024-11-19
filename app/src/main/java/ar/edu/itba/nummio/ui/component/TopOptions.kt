e63c8a4c68cb78ad83be0831f1bd5f114f2e44ff
Ignacio Pedemonte Berthoud <ipedemonteb@gmail.com>
2024-11-19 16:08:26 -0300

    feat: add transaltion and fix top buttons position

--- app/src/main/java/ar/edu/itba/nummio/ui/component/TopOptions.kt
+++ app/src/main/java/ar/edu/itba/nummio/ui/component/TopOptions.kt
@@ -9,20 +9,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.AccountCircle
-import androidx.compose.material.icons.outlined.Add
-import androidx.compose.material.icons.outlined.ArrowForward
-import androidx.compose.material.icons.outlined.Refresh
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,7 +26,7 @@ import ar.edu.itba.nummio.ui.theme.NummioTheme
 
 @Composable
 fun TopOptions() {
-    Box(modifier = Modifier.padding(horizontal = 20.dp)) {
+    Box(modifier = Modifier.padding(horizontal = 12.dp)) {
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically,
@@ -64,13 +57,16 @@ fun RoundOption(
 ) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.size(
+            width = 100.dp,
+            height = 110.dp)
     ) {
         Box(
             contentAlignment = Alignment.Center,
             modifier = Modifier
                 .size(64.dp)
                 .background(DarkPurple, CircleShape)
-                //.clickable(onClick = { onNavigateToRoute("other/33") })
+            //.clickable(onClick = { onNavigateToRoute("other/33") })
         ) {
             Icon(
                 painter = icon,
