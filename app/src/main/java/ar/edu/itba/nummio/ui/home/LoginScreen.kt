0afd35196fedbab1fd835fd893c52fda4839805e
martinAleB <martbarr@gmail.com>
2024-11-20 14:25:55 -0300

    feat: add previews

--- app/src/main/java/ar/edu/itba/nummio/ui/home/LoginScreen.kt
+++ app/src/main/java/ar/edu/itba/nummio/ui/home/LoginScreen.kt
@@ -1,6 +1,10 @@
 package ar.edu.itba.nummio.ui.home
 
+import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import ar.edu.itba.nummio.ui.component.LoginSignupForm
 
 @Composable
@@ -8,8 +12,15 @@ fun LoginScreen(
     onNavigateToRoute: (route: String) -> Unit
 ) {
     LoginSignupForm(
+        modifier = Modifier.background(Color.White),
         title = "Iniciá sesión",
         onConfirmClick = {},
         onCancelClick = { onNavigateToRoute("start") }
     )
+}
+
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen(onNavigateToRoute = {})
 }
