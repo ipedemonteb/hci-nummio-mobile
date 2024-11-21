9e02f2c0019d6566d7a1490a7814ced6072c6842
Ignacio Pedemonte Berthoud <ipedemonteb@gmail.com>
2024-11-21 18:29:14 -0300

    feat: add generate payment screen

--- app/src/main/java/ar/edu/itba/nummio/ui/component/GeneratePayment.kt
+++ app/src/main/java/ar/edu/itba/nummio/ui/component/GeneratePayment.kt
@@ -10,10 +10,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,25 +24,19 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ar.edu.itba.nummio.R
 import ar.edu.itba.nummio.ui.theme.DarkPurple
-import ar.edu.itba.nummio.ui.theme.LightPurple
 import ar.edu.itba.nummio.ui.theme.NummioTheme
 
 @Composable
 fun GeneratePayment() {
     var amount by remember { mutableStateOf("") }
-    var selectedOption by remember { mutableStateOf("") }
     var expanded by remember { mutableStateOf(false) }
-    val options = listOf(
-        stringResource(R.string.concept_various),
-        stringResource(R.string.concept_sale),
-        stringResource(R.string.concept_consumption)
-        )
     val date = remember { mutableStateOf(TextFieldValue("")) }
 
     Column(modifier = Modifier.fillMaxWidth()) {
@@ -59,12 +53,19 @@ fun GeneratePayment() {
                 Row {
                     OutlinedTextField(
                         value = amount,
-                        onValueChange = { amount = it },
+                        onValueChange = { input ->
+                            val sanitizedInput = input.replace("[^\\d,]".toRegex(), "")
+                            if (sanitizedInput.count { it == '.' } <= 1) {
+                                amount = sanitizedInput
+                            }
+                        },
                         placeholder = { Text(text = stringResource(R.string.amount), color = Color.Gray) },
                         modifier = Modifier
                             .fillMaxWidth()
                             .background(Color.White),
-                        singleLine = true
+                        singleLine = true,
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                        keyboardActions = KeyboardActions.Default
                     )
                 }
             }
@@ -99,7 +100,7 @@ fun GeneratePayment() {
             Column {
                 Row {
                     Text(
-                        text = stringResource(R.string.enter_cvu),
+                        text = stringResource(R.string.check_cvu),
                         color = DarkPurple,
                         fontSize = 16.sp
                     )
@@ -119,26 +120,13 @@ fun GeneratePayment() {
                 }
             }
         }
-
-
         Spacer(modifier = Modifier.height(50.dp))
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center
         ) {
-            Button(
-                onClick = {},
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color.White,
-                    contentColor = DarkPurple,
-                    disabledContainerColor = DarkPurple,
-                    disabledContentColor = LightPurple
-                ),
-                modifier = Modifier.width(180.dp).border(1.dp, DarkPurple, RoundedCornerShape(30.dp))
-            ) {
-                Text (
-                    text = stringResource(R.string.generate_link),
-                )
+            Box(modifier = Modifier.padding(horizontal = 60.dp)) {
+                HighContrastBtn(onClick = {}, stringResource(R.string.generate_link))
             }
         }
 
