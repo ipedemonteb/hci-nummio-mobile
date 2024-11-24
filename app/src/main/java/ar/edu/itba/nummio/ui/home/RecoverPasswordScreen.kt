7e44ce0493410c504cbcc2aaceb8a02be7ce0241
leoWeitz <lweitz@itba.edu.ar>
2024-11-24 16:57:44 -0300

    fix: recover password screen is scrollable

--- app/src/main/java/ar/edu/itba/nummio/ui/home/RecoverPasswordScreen.kt
+++ app/src/main/java/ar/edu/itba/nummio/ui/home/RecoverPasswordScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -85,7 +87,8 @@ fun RecoverPasswordScreen(
         }, viewModel = viewModel)}
     ){
         paddingValues ->
-        Column(modifier = Modifier.padding(vertical = 30.dp, horizontal = 30.dp).padding(paddingValues)) {
+        Column(modifier = Modifier.padding(vertical = 30.dp, horizontal = 30.dp).padding(paddingValues).verticalScroll(
+            rememberScrollState())) {
             Row(modifier = Modifier.padding(top = 20.dp)) {
                 Text(
                     text = stringResource(R.string.enter_your_email),
