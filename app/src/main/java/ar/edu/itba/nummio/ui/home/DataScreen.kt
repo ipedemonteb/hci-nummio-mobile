16ff921dcd033d7d6686374a18e55ba27b9f81fd
cgarros33 <cgarros@itba.edu.ar>
2024-11-24 01:27:19 -0300

    feat: share data button

--- app/src/main/java/ar/edu/itba/nummio/ui/home/DataScreen.kt
+++ app/src/main/java/ar/edu/itba/nummio/ui/home/DataScreen.kt
@@ -2,6 +2,7 @@ package ar.edu.itba.nummio.ui.home
 
 import android.content.ClipboardManager
 import android.content.Context
+import android.content.Intent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -38,6 +39,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ar.edu.itba.nummio.R
+import ar.edu.itba.nummio.data.model.AliasRequest
 import ar.edu.itba.nummio.ui.component.CopyableTextInput
 import ar.edu.itba.nummio.ui.component.HighContrastBtn
 import ar.edu.itba.nummio.ui.component.LowContrastBtn
@@ -58,18 +60,28 @@ fun DataScreen(
     }
     val uiState = viewModel.uiState
     Scaffold(
-        topBar = { TopBar(title = stringResource(R.string.my_data), onBackClick = {onBackClick()}, viewModel = viewModel) }
+        topBar = {
+            TopBar(
+                title = stringResource(R.string.my_data),
+                onBackClick = { onBackClick() },
+                viewModel = viewModel
+            )
+        }
     ) {
 
-        paddingValues ->
-        var alias = remember{ mutableStateOf(viewModel.uiState.walletDetails?.alias) }
+            paddingValues ->
+        var alias = remember { mutableStateOf(viewModel.uiState.walletDetails?.alias) }
         val context = LocalContext.current
         if (viewModel.uiState.shouldUpdateWalletDetails) {
             viewModel.getDetails()
         }
         Column(
             modifier = Modifier
-                .padding(horizontal = if(uiState.isLandscape) 76.dp else {if (viewModel.uiState.isOver600dp) 50.dp else 30.dp})
+                .padding(
+                    horizontal = if (uiState.isLandscape) 76.dp else {
+                        if (viewModel.uiState.isOver600dp) 50.dp else 30.dp
+                    }
+                )
                 .padding(paddingValues)
                 .verticalScroll(
                     enabled = uiState.isLandscape,
@@ -86,7 +98,8 @@ fun DataScreen(
                 Image(
                     painter = painterResource(R.drawable.pfp),
                     contentDescription = null,
-                    modifier = Modifier.size(50.dp)
+                    modifier = Modifier
+                        .size(50.dp)
                         .clip(CircleShape)
                         .border(2.dp, DarkPurple, CircleShape)
                 )
@@ -107,7 +120,7 @@ fun DataScreen(
             }
             Spacer(modifier = Modifier.height(8.dp))
             Row {
-                CopyableTextInput(viewModel.uiState.walletDetails?.cbu?:"", false)
+                CopyableTextInput(viewModel.uiState.walletDetails?.cbu ?: "", false)
             }
             Spacer(modifier = Modifier.height(20.dp))
             Row {
@@ -117,23 +130,65 @@ fun DataScreen(
                 )
             }
             Spacer(modifier = Modifier.height(8.dp))
-            Row (modifier = Modifier.padding(bottom = 20.dp)){
-                CopyableTextInput(viewModel.uiState.walletDetails?.alias?:"", true)
+            Row(modifier = Modifier.padding(bottom = 20.dp)) {
+                CopyableTextInput(viewModel.uiState.walletDetails?.alias ?: "", true)
             }
             Spacer(modifier = Modifier.height(32.dp))
             Row(
-                modifier = Modifier.fillMaxWidth().padding(horizontal = if (viewModel.uiState.isOver600dp) 200.dp else 60.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = if (viewModel.uiState.isOver600dp) 200.dp else 60.dp),
                 horizontalArrangement = Arrangement.Center
             ) {
                 LowContrastBtn(onClick = {
-                    val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-                    val combinedText = "CVU: ${viewModel.uiState.walletDetails?.cbu}\nAlias: ${viewModel.uiState.walletDetails?.alias}"
-                    val clip = android.content.ClipData.newPlainText("copiar_dos_valores", combinedText)
-                    clipboard.setPrimaryClip(clip)}, text = stringResource(R.string.copy_all_data))
+                    val clipboard =
+                        context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                    val combinedText =
+                        "CVU: ${viewModel.uiState.walletDetails?.cbu}\nAlias: ${viewModel.uiState.walletDetails?.alias}"
+                    val clip =
+                        android.content.ClipData.newPlainText("copiar_dos_valores", combinedText)
+                    clipboard.setPrimaryClip(clip)
+                }, text = stringResource(R.string.copy_all_data))
+
             }
-            Spacer(modifier = Modifier.height(40.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = if (viewModel.uiState.isOver600dp) 200.dp else 60.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                LowContrastBtn(onClick = {
+                    shareText(
+                        context,
+                        "CVU: ${viewModel.uiState.walletDetails?.cbu}\nAlias: ${viewModel.uiState.walletDetails?.alias}"
+                    )
+                }, text = stringResource(R.string.share_all_data))
+                Spacer(modifier = Modifier.height(40.dp))
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = if (viewModel.uiState.isOver600dp) 200.dp else 60.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                HighContrastBtn(onClick = {
+                    viewModel.updateAlias(AliasRequest(alias.value!!))
+                }, text = stringResource(R.string.update_alias))
+                Spacer(modifier = Modifier.height(40.dp))
+            }
         }
     }
+}
+
+fun shareText(context: android.content.Context, text: String) {
+    val intent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_TEXT, text)
+    }
+
+    // Show the chooser to the user
+    context.startActivity(Intent.createChooser(intent, "Share via"))
 }
 /*
 
